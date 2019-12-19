53ce421d14cf240a5fbb98695accd2af70fdfd58
dsantos22 <dsantos@ibscards.com.au>
2019-12-19 20:43:11 +0800

    Updates

---
+++ precache-manifest.12a4462c0eb44ec2e6101c038d96969a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31058e82d5cb517ded36a7c90c52adfc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "80ce5bbbf9eb410d25a2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "12fc2868a45b1d804101",
+    "url": "/robofriends/static/css/main.e7c37f49.chunk.css"
+  },
+  {
+    "revision": "80ce5bbbf9eb410d25a2",
+    "url": "/robofriends/static/js/2.6d2b6324.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.6d2b6324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12fc2868a45b1d804101",
+    "url": "/robofriends/static/js/main.cee3c269.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
